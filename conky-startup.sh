3c401f23d0f166833ac0928d43f8b5f15fb4e5e2
cerebrux <cerebrux.gr@gmail.com>
2017-06-22 00:24:21 +0300

    Hello world

---
+++ conky-startup.sh
@@ -0,0 +1,8 @@
+sleep 10s
+killall conky
+cd "/home/salih/.conky/Gotham"
+conky -c "/home/salih/.conky/Gotham/Gotham" &
+cd "/home/salih/.conky/TeejeeTech"
+conky -c "/home/salih/.conky/TeejeeTech/Network Panel" &
+cd "/home/salih/.conky/TeejeeTech"
+conky -c "/home/salih/.conky/TeejeeTech/Process Panel" &
